db5114d7654c27910519466243f03d176ef110c5
HeeJaeKK <agmlwo@naver.com>
2021-05-02 19:45:55 +0900

    RacingCalculator : 클래스 생성 및 테스트 성공

---
+++ src/test/java/RacingCalculatorTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class RacingCalculatorTest {
+
+    @Test
+    public void beforeSet() {
+        RacingCalculator racingCalculator = new RacingCalculator();
+    }
+}
