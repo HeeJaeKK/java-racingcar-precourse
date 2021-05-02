9b35f2bac74e5fed9d05cacd65e5a354a58b3655
HeeJaeKK <agmlwo@naver.com>
2021-05-02 22:02:21 +0900

    RacingCalculatePrinter : 클래스 생성 및 테스트

---
+++ src/test/java/RacingCalculatePrinterTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+public class RacingCalculatePrinterTest {
+
+    private RacingCalculatePrinter racingCalculatePrinter;
+
+    @Test
+    public void beforeSet() {
+        racingCalculatePrinter = new RacingCalculatePrinter();
+    }
+}
