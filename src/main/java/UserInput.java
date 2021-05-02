5ea63eb4c95f551459a357108bee1d0e26aaac36
HeeJaeKK <agmlwo@naver.com>
2021-05-02 21:02:25 +0900

    UserInput : 사용자 입력에 따른 레이싱카 할당 테스트 성공

--- src/main/java/UserInput.java
+++ src/main/java/UserInput.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserInput {
+
+    public RacingCar[] allocateRacingCars(String racingCarNames) {
+        List<RacingCar> racingCarList = new ArrayList<>();
+        for (String racingCarName : racingCarNames.split(","))
+            racingCarList.add(new RacingCar(racingCarName));
+        return racingCarList.toArray(new RacingCar[0]);
+    }
+
 }
--- src/test/java/UserInputTest.java
+++ src/test/java/UserInputTest.java
@@ -1,9 +1,20 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class UserInputTest {
 
-    @Test
+    private UserInput userInput;
+    @BeforeEach
     public void beforeSet() {
-        UserInput userInput = new UserInput();
+        userInput = new UserInput();
+    }
+
+    @Test
+    public void allocateRacingCarsTest() {
+        RacingCar[] racingCars = userInput.allocateRacingCars("Test1,Test2,Test123");
+        Assertions.assertEquals(racingCars[0].getCarName(),"Test1");
+        Assertions.assertEquals(racingCars[1].getCarName(),"Test2");
+        Assertions.assertEquals(racingCars[2].getCarName(),"FALSE");
     }
 }
