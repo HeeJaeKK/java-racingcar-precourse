64a5cc3c15593a403032c90e54af4f3158cfe580
HeeJaeKK <agmlwo@naver.com>
2021-05-03 21:44:32 +0900

    UserInput : 리팩토링

--- src/main/java/UserInput.java
+++ src/main/java/UserInput.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class UserInput {
 
@@ -10,22 +8,6 @@ public class UserInput {
 
     public UserInput() {
         br = new BufferedReader(new InputStreamReader(System.in));
-    }
-
-    public RacingCar[] allocateRacingCars(String racingCarNames) {
-        List<RacingCar> racingCarList = new ArrayList<>();
-        for (String racingCarName : racingCarNames.split(","))
-            racingCarList.add(new RacingCar(racingCarName));
-        return racingCarList.toArray(new RacingCar[0]);
-    }
-
-    public boolean validateUserInputCarName(RacingCar[] racingCars) {
-        boolean isValidated = true;
-        for(RacingCar racingCar : racingCars)
-            isValidated = isValidated && !racingCar.getCarName().equals("FALSE");
-        if(!isValidated)
-            System.out.println("자동차 이름의 길이가 제한을 벗어났습니다.");
-        return isValidated;
     }
 
     public String read() {
@@ -38,41 +20,13 @@ public class UserInput {
         return input;
     }
 
-    public int compareStr(RacingCar[] racingCars, String compareStr) {
-        int SameCnt = 0;
-        for(RacingCar racingCar : racingCars)
-            SameCnt += racingCar.getCarName().equals(compareStr) ? 1 : 0;
-        return SameCnt;
-    }
-
-    public boolean validateCarNameUnique(RacingCar[] racingCars) {
-        int SameCnt = 0;
-        for(RacingCar racingCar : racingCars)
-            SameCnt += compareStr(racingCars, racingCar.getCarName());
-        if(SameCnt == racingCars.length)
-            return true;
-        System.out.println("중복된 자동차 이름이 있습니다.");
-        return false;
-    }
-
-    public boolean validateUserCarInput(RacingCar[] racingCars, String userInput) {
-        if(!validateUserInputCarName(racingCars))
-            return false;
-        if(!validateCarNameUnique(racingCars))
-            return false;
-        return true;
-    }
-
-    public RacingCar[] userRacingCarInput() {
-        boolean isValidatedInput = false;
-        RacingCar[] racingCarsInput = new RacingCar[] {};
-        while(!isValidatedInput) {
+    public RacingCars userRacingCarInput() {
+        RacingCars racingCars = new RacingCars("Initialization");
+        while(!racingCars.isRacingCarsAvail()) {
             System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-            String input = read();
-            racingCarsInput = allocateRacingCars(input);
-            isValidatedInput = validateUserCarInput(racingCarsInput, input);
+            racingCars = new RacingCars(read());
         }
-        return racingCarsInput;
+        return racingCars;
     }
 
     public boolean validateUserInputTryNum(String userInputTryNum) {
--- src/test/java/UserInputTest.java
+++ src/test/java/UserInputTest.java
@@ -12,28 +12,8 @@ public class UserInputTest {
     }
 
     @Test
-    public void validateUserInputCarNameTest() {
-        RacingCar racingCarTest1 = new RacingCar("Test1");
-        RacingCar racingCarTest2 = new RacingCar("Test2");
-        Assertions.assertTrue(userInput.validateUserInputCarName(new RacingCar[] {racingCarTest1, racingCarTest2}));
-        RacingCar racingCarTest3 = new RacingCar("Test123");
-        Assertions.assertFalse(userInput.validateUserInputCarName(new RacingCar[] {racingCarTest1, racingCarTest2, racingCarTest3}));
-        RacingCar racingCarTest4 = new RacingCar("");
-        Assertions.assertFalse(userInput.validateUserInputCarName(new RacingCar[] {racingCarTest1, racingCarTest2, racingCarTest4}));
-    }
-
-    @Test
     public void validateUserInputTryNumTest() {
         Assertions.assertTrue(userInput.validateUserInputTryNum("10"));
         Assertions.assertFalse(userInput.validateUserInputTryNum("dfa"));
-    }
-
-    @Test
-    public void validateUserInputCarNameUnique() {
-        RacingCar racingCarTest1 = new RacingCar("Test1");
-        RacingCar racingCarTest2 = new RacingCar("Test2");
-        Assertions.assertTrue(userInput.validateCarNameUnique(new RacingCar[] {racingCarTest1, racingCarTest2}));
-        RacingCar racingCarTest3 = new RacingCar("Test2");
-        Assertions.assertFalse(userInput.validateCarNameUnique(new RacingCar[] {racingCarTest1, racingCarTest2, racingCarTest3}));
     }
 }
